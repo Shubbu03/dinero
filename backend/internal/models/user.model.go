3fad4f83e68697d7d6976c431cc592969e33683b
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-06-07 16:12:48 +0530

    update user model

--- backend/internal/models/user.model.go
+++ backend/internal/models/user.model.go
@@ -8,8 +8,11 @@ type User struct {
 	gorm.Model
 	Name         string        `gorm:"not null"`
 	Email        string        `gorm:"uniqueIndex;not null"`
-	Password     []byte        `gorm:"not null"`
-	Balance      float64       `gorm:"default:0"`
+	Password     []byte        `gorm:"default:null"`
+	AuthProvider string        `gorm:"default:'email'"`
+	ExternalID   string        `gorm:"default:null"`
+	Balance      int64         `gorm:"not null;default:0"`
+	Avatar       string        `gorm:"default:null"`
 	Friends      []*User       `gorm:"many2many:user_friends;joinForeignKey:UserID;joinReferences:FriendID"`
 	Transactions []Transaction `gorm:"foreignKey:SenderID"`
 }
