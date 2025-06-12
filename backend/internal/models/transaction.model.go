aa23f074faa849ae6361eefbd8022319c329ea48
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-06-12 09:29:39 +0530

    updated transaction model

--- backend/internal/models/transaction.model.go
+++ backend/internal/models/transaction.model.go
@@ -6,14 +6,35 @@ import (
 	"gorm.io/gorm"
 )
 
+type TransactionType string
+type PaymentMethod string
+
+const (
+	TransactionSent     TransactionType = "sent"
+	TransactionReceived TransactionType = "received"
+	TransactionSelf     TransactionType = "self"
+)
+
+const (
+	PaymentMethodBalance PaymentMethod = "balance"
+	PaymentMethodCard    PaymentMethod = "card"
+	PaymentMethodUPI     PaymentMethod = "upi"
+)
+
 type Transaction struct {
 	gorm.Model
-	SenderID    uint
-	ReceiverID  uint
-	Amount      int64 `gorm:"not null"`
-	Description string
-	Timestamp   time.Time `gorm:"autoCreateTime"`
+	SenderID      uint
+	ReceiverID    uint
+	Amount        int64 `gorm:"not null"`
+	Fee           int64 `gorm:"default:0"`
+	Description   string
+	Type          TransactionType `gorm:"type:varchar(20);not null"`
+	PaymentMethod PaymentMethod   `gorm:"type:varchar(20);default:'balance'"`
+	CardID        *uint
+	Status        string    `gorm:"default:'completed'"`
+	Timestamp     time.Time `gorm:"autoCreateTime"`
 
-	Sender   User `gorm:"foreignKey:SenderID"`
-	Receiver User `gorm:"foreignKey:ReceiverID"`
+	Sender   *User `gorm:"foreignKey:SenderID"`
+	Receiver User  `gorm:"foreignKey:ReceiverID"`
+	Card     *Card `gorm:"foreignKey:CardID"`
 }
