e98aea5f68f677fb9f92620ee422dd706c0fcdd4
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-06-12 16:55:09 +0530

    add Currency field to User model with default value 'USD'

--- backend/internal/models/user.model.go
+++ backend/internal/models/user.model.go
@@ -11,7 +11,8 @@ type User struct {
 	Password     []byte
 	AuthProvider string `gorm:"default:'email'"`
 	ExternalID   string
-	Balance      int64 `gorm:"not null;default:0"`
+	Balance      int64  `gorm:"not null;default:0"`
+	Currency     string `gorm:"default:'USD'"`
 	Avatar       string
 	Friends      []*User       `gorm:"many2many:user_friends;joinForeignKey:UserID;joinReferences:FriendID"`
 	Transactions []Transaction `gorm:"foreignKey:SenderID"`
